7f5ad16dfc9c581e7568329bededa22f0299c521
Dan Sell <dan.s.sell@gmail.com>
2014-03-29 23:54:04 -0700

    cleanup, add jade, stylus

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      html: {
+        src: ['client/jade/partials/*.jade'],
+        dest: ['client/app/partials']
+      },
+      options: {
+        client: false,
+        pretty: true,
+        expand: true
+      }
+    },
+    stylus: {
+      compile: {
+        files: {
+          'app/css/master.css': ['app/stylus/*.styl']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-jade');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jade', 'stylus'])
+};
